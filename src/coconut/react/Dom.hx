e9e36498e9c8188c59aaaf1f04ff57dd1f4aeecd
Juraj Kirchheim <back2dos@googlemail.com>
2017-05-24 19:16:52 +0200

    Simplify things a little.

---
+++ src/coconut/react/Dom.hx
@@ -0,0 +1,42 @@
+package coconut.react;
+
+import react.ReactComponent.ReactElement;
+import react.ReactMacro.jsx;
+
+abstract Child(Dynamic) from ReactElement from String from Int from Float from Bool {
+
+  @:from static function ofView(view:coconut.ui.BaseView):Child {
+    return jsx('<Wrapper key=${view.id} view=${view} />');
+  }
+
+}
+
+@:native('Object')
+extern class ES6Object {
+  static function assign(rest:haxe.extern.Rest<Dynamic>):Dynamic;
+}
+
+class Dom {
+  static function props(attr, children:Array<Child>):Dynamic {
+    var ret:Dynamic = ES6Object.assign(
+      attr, 
+      if (children != null) { children: children } else null
+    );
+    untyped __js__('delete')(ret.key);
+    return ret;
+  }
+
+  static public function node(tag:String, attr:Dynamic, children:Array<Child>):ReactElement
+    return {
+      "$$typeof": untyped __js__('Symbol.for("react.element")'),
+      type: 'div',
+      key: attr.key,
+      props: props(attr, children),
+    }
+
+  static public inline function div(attr:Dynamic, ?children)
+    return node('div', attr, children);
+
+  static public inline function span(attr:Dynamic, ?children)
+    return node('span', attr, children);
+}
--- tests/Playground.hx
+++ tests/Playground.hx
@@ -2,6 +2,7 @@ package;
 
 import coconut.react.Wrapper;
 import coconut.ui.View;
+import coconut.react.Dom.*;
 import coconut.Ui.hxx;
 import js.Browser.*;
 import react.React;
@@ -9,63 +10,33 @@ import react.ReactDOM;
 import react.ReactMacro.jsx;
 
 class Playground {
-	static function main() {
-		
-		ReactDOM.render(jsx('<Wrapper view=${new CoconutView({})}/>'), document.getElementById('app'));
-		
-	}
+  static function main() {
+    
+    ReactDOM.render(jsx('<Wrapper view=${new CoconutView({})}/>'), document.getElementById('app'));
+    
+  }
 }
 
 class CoconutView extends View<{}> {
-	
-	@:state var counter:Int = 0;
-	
-	function render() {
-		return {
-			"$$typeof": untyped __js__('Symbol.for("react.element")'),
-			type: 'div',
-			props: {
-				children: [
-					untyped {
-						"$$typeof": untyped __js__('Symbol.for("react.element")'),
-						type: 'span',
-						props: {
-							children: 'Hello Coconut!',
-						}
-					},
-					new TextView({value: Std.string(counter)}).render({value: Std.string(counter)})
-				]
-			},
-			key: null,
-			ref: null,
-		}
-	}
-	
-	override function init() {
-		var timer = new haxe.Timer(500);
-		timer.run = function() counter = counter + 1;
-	}
-		
+  
+  @:state var counter:Int = 0;
+  
+  function render() 
+    return div({}, [
+	  span({}, ['Hello Coconut!']),
+	  new TextView({value: Std.string(counter)}),
+	]);
+  
+  override function init() {
+    var timer = new haxe.Timer(500);
+    timer.run = function() counter = counter + 1;
+  }
+    
 }
 
 class TextView extends View<{value:String}> {
-	function render() {
-		return {
-			"$$typeof": untyped __js__('Symbol.for("react.element")'),
-			type: 'div',
-			props: {
-				children: {
-					"$$typeof": untyped __js__('Symbol.for("react.element")'),
-					type: 'span',
-					props: {
-						children: value,
-					}
-				}
-			},
-			key: null,
-			ref: null,
-		}
-	}
-	
-	
+  function render() 
+    return div({}, [
+      span({}, [value])
+    ]);
 }
