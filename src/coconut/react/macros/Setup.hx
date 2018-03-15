b8086e66e0997254c8da95653f234f5fcfe4c363
Kevin Leung <kevinresol@gmail.com>
2018-03-15 17:26:48 +0800

    Fix react checker

--- src/coconut/react/macros/Setup.hx
+++ src/coconut/react/macros/Setup.hx
@@ -11,11 +11,16 @@ private class Generator extends coconut.ui.macros.Generator {
   override function instantiate(name:StringAt, isClass:Bool, key:Option<Expr>, attr:Expr, children:Option<Expr>)
     return 
       if (isClass) {
-        var react = macro @:pos(name.pos) coconut.react.React.createComponent(
-          $i{name.value}, $attr
-        );
-        if (react.typeof().isSuccess()) react;
-        else super.instantiate(name, isClass, key, attr, children);
+        
+        function react(attr)
+          return macro @:pos(name.pos) coconut.react.React.createComponent(
+            $i{name.value}, $attr
+          );
+        
+        switch react(macro null).typeof() {
+          case Success(_): react(attr);
+          case Failure(e): super.instantiate(name, isClass, key, attr, children);
+        }
       }
       else super.instantiate(name, isClass, key, attr, children);
 }
