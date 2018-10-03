37fca63e833eda05833d50d8ce7ba938c3ee0419
Juraj Kirchheim <back2dos@googlemail.com>
2018-10-03 18:31:22 +0200

    Let's try that again.

--- src/coconut/react/Key.hx
+++ src/coconut/react/Key.hx
@@ -1,6 +1,8 @@
 package coconut.react;
 
 abstract Key(Dynamic) from String from Bool from Float from Int {
-  @:from static function ofObj(o:{}):Key
-    return untyped haxe.ds.ObjectMap.getId(o) || haxe.ds.ObjectMap.assignId(o);
+  @:keep @:from static function ofObj(o:{}):Key untyped {
+    var id = haxe.ds.ObjectMap.getId(o) || haxe.ds.ObjectMap.assignId(o);
+    return id;
+  }
 }
