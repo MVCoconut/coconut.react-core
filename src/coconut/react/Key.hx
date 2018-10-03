cab4ea0860cd6fd7b18f5559c21789ccd2bbb50c
Juraj Kirchheim <back2dos@googlemail.com>
2018-10-03 18:23:22 +0200

    Correct treatment of non-primitive keys.

--- src/coconut/react/Key.hx
+++ src/coconut/react/Key.hx
@@ -1,3 +1,6 @@
 package coconut.react;
 
-typedef Key = Dynamic;//no idea
+abstract Key(Dynamic) from String from Bool from Float from Int {
+  @:from static function ofObj(o:{}):Key
+    return untyped haxe.ds.ObjectMap.getId(o) || haxe.ds.ObjectMap.assignId(o);
+}
