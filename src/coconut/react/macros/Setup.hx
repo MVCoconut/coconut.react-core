ce5af60a0c7d2c6fa31625269b5519f7a06ec567
Juraj Kirchheim <back2dos@googlemail.com>
2018-10-28 18:30:09 +0100

    Avoid adding key twice and don't cause class build loop.

--- src/coconut/react/macros/Setup.hx
+++ src/coconut/react/macros/Setup.hx
@@ -24,55 +24,63 @@ class Setup {
 
     for (f in fields)
       if (f.name == 'fromHxx') return null;
-
-    function without(type:haxe.macro.Type, name:String):tink.core.Outcome<haxe.macro.Type, tink.core.Error> {
-      return switch type.reduce() {
-        case TAnonymous(_.get().fields => fields):
-          var fields = fields.filter(function(field) return field.name != name);
-          ComplexType.TAnonymous([for(f in fields) {
-            meta: f.meta.get(),
-            kind: FVar(f.type.toComplex()),
-            name: f.name,
-            pos: f.pos,
-          }]).toType();
-        case TDynamic(null):
-          Success(type);
-        case v:
-          Failure(new tink.core.Error('Cannot remove field from $v'));
-      }
-    }
     
     var type = TPath(cls.name.asTypePath([for (p in cls.params) TPType(p.name.asComplexType())]));
-    var props = switch without((macro (null:$type).props).typeof().sure(), 'children').sure() {
-      case TDynamic(null): macro : Dynamic;
-      case t: [
-        t.toComplex(),
-        macro : {
-          @:optional var key(default, never):coconut.react.Key;
-        }
-      ].intersect().sure();
-    }
     
-    function infer() {
-      return (macro (props : $props));
+    var children = null,
+        childrenOptional = true;//TODO: this flag is actually not evaluated
+
+    var props = switch (macro (null:$type).props).typeof().sure().reduce() {
+      case TDynamic(null):
+        children = macro : coconut.ui.Children;
+        macro : Dynamic;
+      case TAnonymous(_.get().fields => fields):
+        
+        var ret:Array<Field> = [],
+            hasKey = false;
+
+        for (f in fields) 
+          switch f.name {
+            case 'children':
+
+              childrenOptional = f.meta.has(':optional');
+              children = f.type.toComplex();
+
+            default:
+
+              if (f.name == 'key')
+                hasKey = true;
+
+              ret.push({
+                meta: f.meta.get(),
+                kind: FVar(f.type.toComplex()),
+                name: f.name,
+                pos: f.pos,                
+              });
+          }
+
+        if (!hasKey)
+          ret.push((macro class {
+            @:optional var key(default, never):coconut.react.Key;
+          }).fields[0]);
+
+        TAnonymous(ret);
+      case t:     
+        cls.pos.error('unsupported prop type $t'); 
     }
 
-    var add = (switch (macro (null:$type).props.children).typeof() {
-      case Success(_.toComplex() => children):
+    var add = (
+      if (children == null)
         macro class {
-          inline static public function fromHxx(props:$props, ?children:$children):react.ReactComponent.ReactSingleFragment {
-            ${infer.bounce()};
-            return (cast react.React.createElement).apply(react.React, [$i{cls.name}, untyped props].concat(untyped children));
-          }
-        }
-      case Failure(_):
-        macro class {
-          inline static public function fromHxx(props:$props):react.ReactComponent.ReactSingleFragment {
-            ${infer.bounce()};
+          inline static public function fromHxx(props:$props):react.ReactComponent.ReactSingleFragment 
             return cast react.React.createElement($i{cls.name}, props);
-          }
         }
-    }).fields;
+      else 
+        macro class {
+          inline static public function fromHxx(props:$props, ?children:$children):react.ReactComponent.ReactSingleFragment 
+            return (cast react.React.createElement).apply(react.React, [$i{cls.name}, untyped props].concat(untyped children));
+        }      
+    ).fields;
 
     {
       var fromHxx = (add[0]:Member).getFunction().sure();
@@ -104,6 +112,8 @@ class Setup {
       comp.meta.add(':hxxAugmented', [], (macro null).pos);
       comp.meta.add(':autoBuild', [macro coconut.react.macros.Setup.hxxAugment()], (macro null).pos);
     }
+
+    Context.getType('coconut.ui.View').getFields();
 
     coconut.ui.macros.ViewBuilder.afterBuild.whenever(function (ctx) {
       var cls = ctx.target.target;
