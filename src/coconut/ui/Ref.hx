5bdcd786f918dc55f1a0a18c051211a4e6a9d16e
Juraj Kirchheim <back2dos@googlemail.com>
2018-12-28 11:20:14 +0100

    Minor cleanup.

--- src/coconut/ui/Ref.hx
+++ src/coconut/ui/Ref.hx
@@ -2,15 +2,13 @@ package coconut.ui;
 
 import react.*;
 
-abstract Ref<T>(ReactRef<T>) {//TODO: make this an extern and use React.createRef
+abstract Ref<T>(ReactRef<T>) {
   public var current(get, never):T;
   inline function get_current():T 
     return this.current;
   public inline function new()
     this = React.createRef();
 }
-
-
 
 abstract RefSetter<T>(T->Void) from T->Void {
   @:from static inline function ofRef<T>(r:Ref<T>):RefSetter<T>
