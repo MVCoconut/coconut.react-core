9a1f84f0f55bb85a25b8a0ef1b156610104d0b01
Juraj Kirchheim <back2dos@googlemail.com>
2019-02-05 14:13:32 +0100

    Update dependencies.

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -1,5 +1,7 @@
 package ;
 
+import coconut.ui.*;
+
 class RunTests {
 
   static function main() {
@@ -7,4 +9,10 @@ class RunTests {
     travix.Logger.exit(0); // make sure we exit properly, which is necessary on some targets, e.g. flash & (phantom)js
   }
   
+}
+
+class Foo extends View {
+  function render() ' 
+    <Isolated>Hello, world!</Isolated>
+  ';
 }
