956b1d585cfcbd61d221212eb02f7b91ef03e00d
Kevin Leung <kevinresol@gmail.com>
2020-03-14 13:10:52 +0800

    Add test

---
+++ tests/HocTest.hx
@@ -0,0 +1,72 @@
+package;
+
+import haxe.Timer;
+import react.React;
+import js.Browser.*;
+import coconut.Ui.hxx;
+import react.ReactType;
+import react.ReactComponent;
+
+using coconut.ui.Renderer;
+
+@:asserts
+class HocTest {
+	public function new() {}
+	
+	public function test() {
+		var div = document.createDivElement();
+		document.body.appendChild(div);
+		
+		var expected = 0;
+		
+		function done() {
+			document.body.removeChild(div);
+			asserts.done();
+		}
+		
+		function update(foo:Int) {
+			asserts.assert(foo == expected++);
+		}
+		
+		Renderer.mount(div, hxx('<Wrapped onUpdate=${update} onFinish=${done}/>'));
+		
+		return asserts;
+	}
+}
+
+@:react.hoc(wrapper)
+class Wrapped extends coconut.ui.View {
+	@:attr var onUpdate:(foo:Int)->Void;
+	@:attr var onFinish:()->Void;
+	
+	@:react.injected var foo:Int;
+	
+	static function wrapper(component:ReactType):ReactType {
+		return function(props)
+			return React.createElement(Injector, js.Object.assign({component: component}, props));
+	}
+	
+	function render() '<div>$foo</div>';
+	
+	function viewDidRender() {
+		onUpdate(foo);
+		if(foo == 5) onFinish();
+	}
+}
+
+class Injector extends ReactComponent {
+	final timer:Timer;
+	
+	public function new(props) {
+		super(props);
+		state = {foo: 0}
+		timer = new Timer(123);
+		timer.run = function() setState({foo: state.foo + 1});
+	}
+	override function render() {
+		return React.createElement(cast props.component, js.Object.assign({foo: state.foo}, props));
+	}
+	override function componentWillUnmount() {
+		timer.stop();
+	}
+}
--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -1,12 +1,15 @@
 package ;
 
 import coconut.ui.*;
+import tink.testrunner.*;
+import tink.unit.*;
 
 class RunTests {
 
   static function main() {
-    travix.Logger.println('it works');
-    travix.Logger.exit(0); // make sure we exit properly, which is necessary on some targets, e.g. flash & (phantom)js
+    Runner.run(TestBatch.make([
+      new HocTest(),
+    ])).handle(Runner.exit);
   }
   
 }
