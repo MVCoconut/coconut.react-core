341463af50c5199b64afcc2d06989f2e16d64a74
Kevin Leung <kevinresol@gmail.com>
2020-06-02 15:42:47 +0800

    HOC rebase

--- src/coconut/react/View.macro.hx
+++ src/coconut/react/View.macro.hx
@@ -15,12 +15,13 @@ class View {
 
     var self = toComplex(cls);
 
-    var attributes = TAnonymous(ctx.attributes.concat(
+    var attributeFields = ctx.attributes.concat(
       (macro class {
         @:optional var key(default, never):coconut.react.Key;
         @:optional var ref(default, never):coconut.ui.Ref<$self>;
       }).fields
-    ));
+    );
+    var attributes = TAnonymous(attributeFields);
 
     {
       var render = ctx.target.memberByName('render').sure();
@@ -48,13 +49,107 @@ class View {
     #if react_devtools
     ctx.target.getConstructor().addStatement(macro this.__stateMap = $stateMap);
     #end
+    
+    
+    var reactType = macro cast $i{ctx.target.target.name};
+      
+    // HOC wrap
+    switch cls.meta.extract(':react.hoc') {
+      case []:
+        // do nothing
+      case wraps:
+        var wrapped = macro cast $i{ctx.target.target.name};
+        
+        for(i in 0...wraps.length) { // loop in reverse, so that the first meta will become the outermost wrap
+          switch wraps[wraps.length - i - 1] {
+            case {params: [wrapper]}:
+              wrapped = macro $wrapper($wrapped);
+            case {params: [wrapper, e = macro (_:$ct)]}: // https://github.com/HaxeFoundation/haxe-evolution/pull/44
+              switch ct.toType() {
+                case Success(_.reduce().toComplex() => TAnonymous(fields)):
+                  for(field in fields) {
+                    switch field.kind {
+                      case FVar(ct, e): field.kind = FVar(macro:coconut.data.Value<$ct>, e);
+                      case FProp(get, set, ct, e): field.kind = FProp(get, set, macro:coconut.data.Value<$ct>, e);
+                      case _: // TODO:
+                    }
+                    attributeFields.push(field);
+                  }
+                case _:
+                  e.pos.error('Expected anonymous structure type');
+              }
+              wrapped = macro $wrapper($wrapped);
+            case {params: [wrapper, _], pos: pos}:
+              pos.error('Second parameter of @:wrap should be a ETypeCheck expr');
+            case {pos: pos}:
+              pos.error('@:wrap must have one or two parameters');
+          }
+        }
+        
+        ctx.target.addMembers(macro class {
+          @:keep static var __hoc:react.ReactType = $wrapped;
+        });
+        
+        reactType = macro $reactType.__hoc;
+    }
+    
+    // injected props
+    var init = 
+      switch ctx.target.memberByName('__initAttributes') {
+        case Success({kind: FFun(f)}): f;
+        case _: throw 'unreachable';
+      }
+    
+    for(member in ctx.target)
+      switch [member.kind, member.meta.filter(function(meta) return meta.name == ':react.injected')] {
+        case [_, []]:
+          // skip
+        case [FFun(_), _]:
+          member.pos.error('@:react.injected does not work on functions');
+        case [FVar(ct, e) | FProp(_, _, ct, e), [meta = {params: params}]]:
+          if(e != null) e.pos.error('Field with @:react.injected cannot have an initializer');
+          var name = switch params {
+            case []:
+              member.name;
+            case [macro $v{(name:String)}]:
+              name;
+            case _:
+              meta.pos.error('@:react.injected should have at most one parameter');
+          }
+          var internal = '__coco_${member.name}';
+          var getter = 'get_${member.name}';
+          
+          member.kind = FProp('get', 'never', ct, null);
+          ctx.target.addMembers(macro class {
+            @:noCompletion private var $internal:tink.state.Observable<$ct> =
+              new tink.state.State(null);
+            inline function $getter() return $i{internal}.value;
+          });
+          
+          init.expr = init.expr.concat(macro {
+            var value:Dynamic = (cast $i{init.args[0].name}).$name;
+            if(($i{internal}:Dynamic) != value) {
+              if(Std.is(value, tink.state.Observable.ObservableObject))
+                $i{internal} = value; // TODO: this is so hacky, fix me please
+              else
+                (cast $i{internal}:tink.state.State<$ct>).set(value);
+            }
+          });
+          
+        case _:
+          member.pos.error('Multiple @:react.injected is not supported');
+      }
+    
+    
+    
+    
 
     var added = ctx.target.addMembers(macro class {
       #if react_devtools
       @:keep @:noCompletion var __stateMap:{};
       #end
       static public function fromHxx(attributes:$attributes):coconut.ui.RenderResult {
-        return cast react.React.createElement(cast $i{ctx.target.target.name}, attributes);
+        return cast react.React.createElement($reactType, attributes);
       }
     });
 
