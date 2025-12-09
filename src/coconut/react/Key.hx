5765d95c836c9f1cf6828751f189601d6cf79126
Juraj Kirchheim <back2dos@gmail.com>
2025-12-09 12:57:41 +0100

    Haxe 5 compat.

--- src/coconut/react/Key.hx
+++ src/coconut/react/Key.hx
@@ -2,9 +2,18 @@ package coconut.react;
 
 abstract Key(Dynamic) from String from Bool from Float from Int #if genes from {} #end {
   #if !genes
-  @:keep @:from static function ofObj(o:{}):Key untyped {
-    var id = haxe.ds.ObjectMap.getId(o) || haxe.ds.ObjectMap.assignId(o);
-    return id;
-  }
+    @:keep @:from static function ofObj(o:{}):Key {
+      var id =
+        #if (js_es >= 6)
+          (cast o)[cast ID] ??= counter++;
+        #else
+          @:privateAccess (haxe.ds.ObjectMap.getId(o) || haxe.ds.ObjectMap.assignId(o));
+        #end
+      return id;
+    }
+    #if (js_es >= 6)
+      static var counter = 0;
+      static final ID = new js.lib.Symbol();
+    #end
   #end
 }
